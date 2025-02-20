0c650d0626e12d36d2d777fb128287827d3e6e49
Andrei De Jesus <andreidejesus06@gmail.com>
2025-02-20 13:51:25 +0800

    initial commit

---
+++ Objects/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace CardFlip.Objects
+{
+    public class Card
+    {
+        public int CardId { get; set; }
+        public string Image { get; set; } = string.Empty;
+        public bool Solved { get; set; }
+    }
+}
