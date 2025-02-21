22392a8b24915b4f8c34b6634625f9957f9dae90
Andrei De Jesus <andreidejesus06@gmail.com>
2025-02-21 16:39:59 +0800

    add localstorage and add scoring feature

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -12,6 +13,8 @@ namespace CardFlip
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
